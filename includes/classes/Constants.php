2e6673192ea4116bd7b07a2c9a7339f7ccf0d3e8
Elinian <33960901+Elinian@users.noreply.github.com>
2018-06-15 21:49:57 -0300

    Add files via upload

---
+++ includes/classes/Constants.php
@@ -0,0 +1,18 @@
+<?php
+class Constants {
+
+	public static $passwordsDoNoMatch = "Senha Incorreta";
+	public static $passwordNotAlphanumeric = "Suas senha deve conter numeros e letras";
+	public static $passwordCharacters = "Sua senha deve conter 5 e 30 caracteres";
+	public static $emailInvalid = "O Email é invalido";
+	public static $emailsDoNotMatch = "Your emails esta incorreto";
+	public static $emailTaken = "Esse email ja esta em uso";
+	public static $lastNameCharacters = "Seu sobrenome deve conter 2 e 25 caracteres";
+	public static $firstNameCharacters = "Seu nome deve conter 2 e 25 caracteres";
+	public static $usernameCharacters = "Seu usuario deve conter 5 e 25 caracteres";
+	public static $usernameTaken = "Esse usuário já existe";
+
+	public static $loginFailed = "Seu usuario ou senha esta incorreto";
+
+}
+?>
